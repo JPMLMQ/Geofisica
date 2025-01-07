fe391bdaee04ffb0d7c567d6b34e2212b589c00c
JPMLMQ <juanmello@id.uff.br>
2025-01-07 11:54:46 -0300

    geometria

--- createGeometry.py
+++ createGeometry.py
@@ -5,26 +5,26 @@ import pandas as pd
 # sources
 sx_init = 100
 sx_end  = 4900
-Nsource = 10
+Nsource = 4800
 sx = np.linspace(sx_init,sx_end,Nsource,endpoint=False)
-sz =  300 * np.ones(len(sx))
+sz = 50 * np.ones(len(sx))
 sIdx = np.arange(Nsource)
 
 # receivers
 rx_init = 0
 rx_end = 5000
-Nrec = 100
+Nrec = 5001
 rx = np.linspace(rx_init,rx_end,Nrec,endpoint=False)
-rz = 60 * np.ones(len(rx))
+rz = np.ones(len(rx))
 rIdx = np.arange(Nrec)
 
 
 plt.figure()
 plt.plot(sx,sz,"r*")
 plt.plot(rx,rz,'bv')
-plt.xlim(0,5000)
-plt.ylim(1000,0)
-#plt.show()
+plt.xlim(0,rx_end)
+plt.ylim(rx_end,0)
+plt.show()
 
 receiver_df = pd.DataFrame({'index': rIdx,'coordx': rx,'coordz': rz})
 
